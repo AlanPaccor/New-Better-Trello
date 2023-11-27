b339515c36c958a03d0c50d239ee76754def18b0
Alan Paccor <alan@Alans-Air.attlocal.net>
2023-11-27 02:32:31 -0500

    made routes public for deployed website

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  // Add a wildcard route to make all routes public
+  publicRoutes: ["/", "/api/webhook", "/*"],
   afterAuth(auth, req) {
     if (auth.userId && auth.isPublicRoute) {
       let path = "/select-org";
@@ -28,8 +26,7 @@ export default authMiddleware({
     }
   }
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
