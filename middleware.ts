8badd9a58adf3ef8aba53653e5d95939272b3ae6
Alan Paccor <alan@Alans-Air.lan1>
2023-12-03 01:00:01 -0500

    made change in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  // Add a wildcard route to make all routes public
-  publicRoutes: ["/", "/api/webhook", "/*"],
+  publicRoutes: ["/", "/api/webhook"],
   afterAuth(auth, req) {
     if (auth.userId && auth.isPublicRoute) {
       let path = "/select-org";
@@ -26,7 +28,8 @@ export default authMiddleware({
     }
   }
 });
-
+ 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
